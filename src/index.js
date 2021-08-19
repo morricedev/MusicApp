6f8810c617bb7eabae3ad64bd1337ebb33cd5f90
Maurício Sousa <morricedev@gmail.com>
2021-08-19 12:52:02 -0300

    Add theme styles and ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
 import App from "./App";
+import { theme, GlobalStyles } from "./styles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
