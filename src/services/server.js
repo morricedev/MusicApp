97aea3215d2fba0cae946480f47b4a67b29c8298
Maurício Sousa <morricedev@gmail.com>
2021-08-20 17:42:12 -0300

    Add CORS Anywhere

---
+++ src/services/server.js
@@ -0,0 +1,13 @@
+var host = "0.0.0.0";
+var port = 8080;
+
+var cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    originWhitelist: [],
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
