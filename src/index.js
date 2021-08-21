10f33c90610230575b328a34791c27c6737bbba7
Maurício Sousa <morricedev@gmail.com>
2021-08-21 12:35:41 -0300

    Add providers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 
 import App from "./App";
 import { theme, GlobalStyles } from "./styles";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
